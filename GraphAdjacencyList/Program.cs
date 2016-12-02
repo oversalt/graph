055070701f0f920b68b869f3fb4003a9210664c5
Railgun55 <brett_larose@live.ca>
2016-12-02 13:52:43 -0600

    Last commit before handing in

--- GraphAdjacencyList/Program.cs
+++ GraphAdjacencyList/Program.cs
@@ -8,56 +8,9 @@ namespace GraphAdjacencyList
 {
     class Program
     {
-        static void TestDirectedGraph()
-        {
-            DGraphAL<string> dGraph = new DGraphAL<string>();
-            dGraph.AddVertex("Saskatoon");
-            dGraph.AddVertex("Moose Jaw");
-            dGraph.AddVertex("Regina");
-            dGraph.AddEdge("Saskatoon", "Moose Jaw", 235);
-            dGraph.AddEdge("Saskatoon", "Regina", 250);
-            dGraph.AddEdge("Regina", "Moose Jaw", 70);
-            //dGraph.RemoveEdge("Saskatoon", "Regina");
-            Console.WriteLine(dGraph);
-
-            //List<Vertex<string>> myL = (List<Vertex<string>>)uGraph.EnumerateNeighbours("Regina");
-
-            //foreach (Vertex<string> v in myL)
-            //{
-            //    Console.WriteLine(v.Data);
-            //}
-
-            //Edge<string>[] edges = Graph.TestGetAllEdges();
-
-            //foreach(var e in edges)
-            //{
-            //    Console.WriteLine(e.ToString());
-            //}
-        }
-
-        static void TestUndirectedRemoveVertex()
-        {
-            UGraphAL<string> uGraph = new UGraphAL<string>();
-            uGraph.AddVertex("Saskatoon");
-            uGraph.AddVertex("Moose Jaw");
-            uGraph.AddVertex("Regina");
-            uGraph.AddEdge("Saskatoon", "Moose Jaw", 235);
-            uGraph.AddEdge("Saskatoon", "Regina", 250);
-            uGraph.AddEdge("Regina", "Moose Jaw", 70);
-            Console.WriteLine(uGraph);
-
-            Console.WriteLine(uGraph.GetEdge("Saskatoon", "Moose Jaw"));
-
-            Console.WriteLine(uGraph);
-            Console.WriteLine(uGraph.NumEdges);
-        }
-        
-
-
         static void Main(string[] args)
         {
-            //TestDirectedGraph();
-            TestUndirectedRemoveVertex();
+            
         }
     }
 }
